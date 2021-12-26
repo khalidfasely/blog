877f93c5ad1537b9dd2a14d19306f368aa78ef2e
khalidfasely <faselykhalid01@gmail.com>
2021-12-26 15:49:05 +0100

    Style Register page

--- blog-f-e/src/components/Register.js
+++ blog-f-e/src/components/Register.js
@@ -53,57 +53,83 @@ export const Register = ({ startRegister }) => {
     }
 
     return (
-        <div>
-            <form data-testid='form' onSubmit={onFormSubmit}>
-                {error && <p data-testid='error_message'>{error}</p>}
-                <label htmlFor='username_new'>Username:</label>
-                <input
-                  id='username_new'
-                  data-testid='username'
-                  name='username'
-                  type='text'
-                  autoFocus
-                  placeholder='Username'
-                  value={username}
-                  maxLength={35}
-                  onChange={onUsernameChange}
-                />
-                <label htmlFor='email_new'>Email:</label>
-                <input
-                  id='email_new'
-                  data-testid='email'
-                  name='email'
-                  type='email'
-                  placeholder='Email'
-                  value={email}
-                  maxLength={70}
-                  onChange={onEmailChange}
-                />
-                <label htmlFor='password_new'>Password:</label>
-                <input
-                  id='password_new'
-                  data-testid='password'
-                  name='password'
-                  type='password'
-                  placeholder='Password'
-                  value={password}
-                  maxLength={120}
-                  onChange={onPasswordChange}
-                />
-                <label htmlFor='confirmation_new'>Confirmation:</label>
-                <input
-                  id='confirmation_new'
-                  data-testid='confirmation'
-                  name='confirmation'
-                  type='password'
-                  placeholder='Password (Again)'
-                  value={confirmation}
-                  maxLength={120}
-                  onChange={onConfirmationChange}
-                />
-                <button data-testid='button'>Register</button>
+        <div className='content-container'>
+            <form className='register-form' data-testid='form' onSubmit={onFormSubmit}>
+                {error && <p className='register-form__error' data-testid='error_message'>{error}</p>}
+                <div className='register-form__lab-inp'>
+                    <div>
+                        <label htmlFor='username_new'>Username:</label>
+                    </div>
+                    <div>
+                        <input
+                          id='username_new'
+                          data-testid='username'
+                          name='username'
+                          type='text'
+                          autoFocus
+                          placeholder='Username'
+                          value={username}
+                          maxLength={35}
+                          onChange={onUsernameChange}
+                        />
+                    </div>
+                </div>
+                <div className='register-form__lab-inp'>
+                    <div>
+                        <label htmlFor='email_new'>Email:</label>
+                    </div>
+                    <div>
+                        <input
+                          id='email_new'
+                          data-testid='email'
+                          name='email'
+                          type='email'
+                          placeholder='Email'
+                          value={email}
+                          maxLength={70}
+                          onChange={onEmailChange}
+                        />
+                    </div>
+                </div>
+                <div className='register-form__lab-inp'>
+                    <div>
+                        <label htmlFor='password_new'>Password:</label>
+                    </div>
+                    <div>
+                        <input
+                          id='password_new'
+                          data-testid='password'
+                          name='password'
+                          type='password'
+                          placeholder='Password'
+                          value={password}
+                          maxLength={120}
+                          onChange={onPasswordChange}
+                        />
+                    </div>
+                </div>
+                <div className='register-form__lab-inp'>
+                    <div>
+                        <label htmlFor='confirmation_new'>Confirmation:</label>
+                    </div>
+                    <div>
+                        <input
+                          id='confirmation_new'
+                          data-testid='confirmation'
+                          name='confirmation'
+                          type='password'
+                          placeholder='Password (Again)'
+                          value={confirmation}
+                          maxLength={120}
+                          onChange={onConfirmationChange}
+                        />
+                    </div>
+                </div>
+                <div>
+                    <button data-testid='button'>Register</button>
+                </div>
+                <div className='register-form__link'>Already have an account? <Link to='/login'>Log In here.</Link></div>
             </form>
-            Already have an account? <Link to='/login'>Log In here.</Link>
         </div>
     );
 };
