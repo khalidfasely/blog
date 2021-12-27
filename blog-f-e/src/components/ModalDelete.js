e812210f3694f5a8ea8e9f772d82503283ca96c6
khalidfasely <faselykhalid01@gmail.com>
2021-12-27 16:50:21 +0100

    Style DeleteModal

--- blog-f-e/src/components/ModalDelete.js
+++ blog-f-e/src/components/ModalDelete.js
@@ -3,14 +3,21 @@ import Modal from 'react-modal';
 
 export const ModalDelete = ({ rModalOpen, deleteBlog, setRModalOpen }) => (
     <Modal
-        isOpen={rModalOpen}
-        contentLabel="Delete Blog"
-        closeTimeoutMS={100}
-        onRequestClose={() => setRModalOpen(false)}
-      >
-        <h5>If you delete this blog there is no way to return it.</h5>
-        <button data-testid='delete_button' onClick={deleteBlog}>Delete Blog</button>
-        <button data-testid='close_button' onClick={() => setRModalOpen(false)}>X</button>
+      className='delete-modal'
+      isOpen={rModalOpen}
+      contentLabel="Delete Blog"
+      closeTimeoutMS={100}
+      onRequestClose={() => setRModalOpen(false)}
+    >
+      <h5 className='delete-modal__title'>If you delete this blog there is no way to return it.</h5>
+      <div className='delete-modal__buttons'>
+        <abbr title='Delete Blog'>
+          <button className='delete-modal__delete-button' data-testid='delete_button' onClick={deleteBlog}>Delete Blog</button>
+        </abbr>
+        <abbr title='Close'>
+          <button className='delete-modal__close-button' data-testid='close_button' onClick={() => setRModalOpen(false)}>X</button>
+        </abbr>
+      </div>
     </Modal>
 );
 
