9746de16a63fa9d9d06d3652f762af3af1d50ced
khalidfasely <faselykhalid01@gmail.com>
2022-01-03 10:50:20 +0100

    Make Changes on Footer

--- blog-f-e/src/components/Footer.js
+++ blog-f-e/src/components/Footer.js
@@ -9,36 +9,24 @@ export const Footer = () => {
                     <div className='footer-col'>
                         <h4>About</h4>
                         <ul>
-                            <li><a>Blog</a></li>
-                            <li><a>Help</a></li>
-                            <li><a>Something</a></li>
-                            <li><a>Something</a></li>
+                            <li><a href='#'>Blog</a></li>
+                            <li><a href='#'>Help</a></li>
                         </ul>
                     </div>
                     <div className='footer-col'>
                         <h4>Other Apps</h4>
                         <ul>
-                            <li><a>Expensify App</a></li>
-                            <li><a>Indecision App</a></li>
-                            <li><a>Network App</a></li>
-                            <li><a>Portfolio</a></li>
-                        </ul>
-                    </div>
-                    <div className='footer-col'>
-                        <h4>Something</h4>
-                        <ul>
-                            <li><a>Something</a></li>
-                            <li><a>Something</a></li>
-                            <li><a>Something</a></li>
-                            <li><a>Something</a></li>
+                            <li><a href='#'>Expensify App</a></li>
+                            <li><a href='#'>Indecision App</a></li>
+                            <li><a href='#'>Network App</a></li>
                         </ul>
                     </div>
                     <div className='footer-col'>
                         <h4>Follow us</h4>
                         <div className='social_links'>
-                            <a><FacebookIcon size={32} round={true} /></a>
-                            <a><TwitterIcon size={32} round={true} /></a>
-                            <a><LinkedinIcon size={32} round={true} /></a>
+                            <a href='#'><FacebookIcon size={32} round={true} /></a>
+                            <a href='#'><TwitterIcon size={32} round={true} /></a>
+                            <a href='#'><LinkedinIcon size={32} round={true} /></a>
                         </div>
                     </div>
                 </div>
